4fc9db9abfdf846c272c8bbd14242a27d7cd496c
elkesauter <elke.m.sauter@gmail.com>
2017-01-11 16:14:58 +0100

    Function Leap
    Addition of explanatory comments

--- Lesson3/R/leap.R
+++ Lesson3/R/leap.R
@@ -4,23 +4,35 @@ leap<- function(x) {
   if(!is.numeric(x)) {
     warning("x must be of class numeric")
     
+  #calculation of leap year
+    
   } else { # x is numeric
+    
+    #calculate modulus of 4,100 and 400
     mod4=x%%4
     mod100=x%%100
     mod400=x%%400
-    yearValue<-FALSE
-    #test<-"test value"
     
+    #assume first all years are not leap years (common years=FALSE) 
+    yearValue<-FALSE
+ 
+    
+    #if year is not divisible by 4 it is a common year 
     if(mod4!=0){
       yearValue<-FALSE
     }
     
+    #if year is not divisible by 100 it is a leap year 
     else if(mod100!=0){
       yearValue<-TRUE
     }
+    
+    #if year is not divisible by 400 it is a common year 
     else if(mod400!=0){
       yearValue<-FALSE
     }
+    
+    #otherwise it is a leap year
     else{
       yearValue<-TRUE
     } 
